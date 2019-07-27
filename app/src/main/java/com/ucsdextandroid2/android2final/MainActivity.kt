5f1bc77ed5689518fd0e68b114e040730817f74e
RJ Aylward <rjaylwar@gmail.com>
2019-07-27 00:22:49 -0700

    setup

---
+++ app/src/main/java/com/ucsdextandroid2/android2final/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ucsdextandroid2.android2final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
